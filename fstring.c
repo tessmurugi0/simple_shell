f77293172451387f717789319e8efb364627249f
Opiyo24 <gabbynyangor@gmail.com>
2023-07-17 13:48:41 +0300

    Some head.... with strings attached

---
+++ fstring.c
@@ -0,0 +1,105 @@
+//#include "shell.h"
+#include <stdio.h>
+#include <string.h>
+
+int f_strlen(char *s);
+int f_strcmp(char *s, char *t);
+char *f_starts_with(const char *haystack, const char *needle);
+char *f_strcat(char *destination, char *source);
+
+/**
+ * f_strlen - loops through a string and returns its length
+ * as an integer
+ * @s: the string to iterate through
+ * 
+ * Return: 0 on success
+*/
+
+int f_strlen(char *s)
+{
+	int counter = 0;
+
+    if (!s)
+    {
+        return(0);
+    }
+
+	while (*s)
+	{
+	    counter++;
+	    *s++;
+    }
+    return(counter);
+}
+
+
+/**
+ * f_strcmp - Loops through two strings comparing
+ * their characters one by one
+ * @s: first string to loop through
+ * @t: second string to loop trough
+ * 
+ * Return: 0 on success
+*/
+
+int f_strcmp(char *s, char *t)
+{
+    int counter = 0;
+
+    while (*s && *t)
+    {
+        if (s[counter] == t[counter])
+        {
+           s[counter]++;
+           t[counter]++;
+        }
+        else
+        {
+            return(1);
+        }
+    }
+    return(0);
+}
+
+
+
+/**
+ * f_starts_with - checks is a string starts witha  specified sub string
+ * @hasytack: the main string to loop through
+ * @needle: sub string to loop through
+ * 
+ * Return: 0 on success
+*/
+
+char *f_starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+
+/**
+ * f_strcat - function that joins two strings, the second
+ * to the first.
+ * @destination: first string onto which the second is joined
+ * @source: the second string provided
+ * 
+ * Return: concatenated string.
+*/
+char *f_strcat(char *destination, char *source)
+{
+    char *result = destination;
+    while (*destination)
+    {
+        destination++;
+    }
+
+    while (*source)
+    {
+        *destination++ = *source++;
+    }
+    *destination = *source;
+    return (result);
+}
---
+++ shell.h
@@ -0,0 +1,42 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+typedef struct info
+{
+    char *arg;
+    char **argv;
+    char *path;
+    int argc;
+    unsigned int line_count;
+    int err_num;
+    int line_count_flag;
+    char *fname;
+    list_t *env;
+    list_t *history;
+    list_t *alias;
+    char **environ;
+    int env_changed;
+    int status;
+
+    char **cmd_buf;
+    int cmd_buf_type;
+    int readfd;
+    int histcount;
+} info_t;
+
+int mode(info_t *info);
+int isdelimeter(char c, char *delimeter);
+int isalpha(int c);
+int _atoi(char *s);
+int f_strlen(char *s);
+int f_strcmp(char *s, char *t);
+char *f_starts_with(const char *haystack, const char *needle);
+char *f_strcat(char *destination, char *source);
+
+#endif
